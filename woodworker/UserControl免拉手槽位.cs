c73f86a30e4d7e590a485efbaf46a62437d828fa
BIGSING <pushebp@163.com>
2025-05-29 23:07:42 +0800

    1

--- woodworker/UserControl免拉手槽位.cs
+++ woodworker/UserControl免拉手槽位.cs
@@ -1,7 +1,7 @@
 ﻿namespace woodworker;
 public partial class UserControl免拉手槽 : UserControl {
     const int 面板外延尺寸 = 10;      // 面板外延尺寸，单位mm
-    const int 槽深 = 22;          // 单位mm
+    const int 槽深 = 20;          // 单位mm
     const int 木板厚度 = 18;                        // 木板厚度为18mm
 
     public UserControl免拉手槽() {
@@ -23,9 +23,9 @@ public partial class UserControl免拉手槽 : UserControl {
 
         var 横条 = new CutPiece("横条");
         横条.长度 = 槽宽;
-        横条.宽度 = 槽深 + 木板厚度;
+        横条.宽度 = 槽深 + 木板厚度 + 木板厚度;
         横条.Quantity = 1;
-        横条.Notes = "横条压竖条";
+        横条.Notes = "横条压竖条，外边层板齐，内侧多一板，好做无钉眼";
         cutPieces.Add(横条);
 
         var 竖条 = new CutPiece("竖条");
@@ -39,7 +39,7 @@ public partial class UserControl免拉手槽 : UserControl {
         ///////////////////////////////////////////
         string result = string.Empty;
         result += $"【免拉手槽】：\r\n槽净高: {槽净高}mm, 槽宽: {槽宽}mm\r\n";
-        result += $"组装方式：横条压竖条；竖条钉层板，可在层板上，可在层板下。\r\n\r\n";
+        result += $"组装方式：横条压竖条，外边层板齐，内侧多一板，好做无钉眼；竖条钉层板，可在层板上，可在层板下。\r\n\r\n";
 
         int index = 1;
         foreach (var cutPiece in cutPieces) {
