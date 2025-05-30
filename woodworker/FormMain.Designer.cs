08b9c6ce3dc698bd1b2719670682ad59340c7077
BIGSINGER <pushebp@163.com>
2025-05-30 18:01:59 +0800

    1

--- woodworker/FormMain.Designer.cs
+++ woodworker/FormMain.Designer.cs
@@ -27,30 +27,54 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            splitContainer1 = new SplitContainer();
             tabControlMain = new TabControl();
             tabPageDesk = new TabPage();
             tabPage2 = new TabPage();
             txtResult = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
             tabControlMain.SuspendLayout();
             SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            splitContainer1.Orientation = Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(tabControlMain);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(txtResult);
+            splitContainer1.Panel2.RightToLeft = RightToLeft.No;
+            splitContainer1.Size = new Size(763, 477);
+            splitContainer1.SplitterDistance = 166;
+            splitContainer1.TabIndex = 9;
             // 
             // tabControlMain
             // 
             tabControlMain.Controls.Add(tabPageDesk);
             tabControlMain.Controls.Add(tabPage2);
-            tabControlMain.Dock = DockStyle.Top;
+            tabControlMain.Dock = DockStyle.Fill;
             tabControlMain.Location = new Point(0, 0);
             tabControlMain.Name = "tabControlMain";
             tabControlMain.SelectedIndex = 0;
-            tabControlMain.Size = new Size(800, 226);
-            tabControlMain.TabIndex = 0;
+            tabControlMain.Size = new Size(763, 166);
+            tabControlMain.TabIndex = 11;
             // 
             // tabPageDesk
             // 
             tabPageDesk.Location = new Point(4, 26);
             tabPageDesk.Name = "tabPageDesk";
             tabPageDesk.Padding = new Padding(3);
-            tabPageDesk.Size = new Size(792, 196);
+            tabPageDesk.Size = new Size(755, 136);
             tabPageDesk.TabIndex = 0;
             tabPageDesk.Text = "书桌";
             tabPageDesk.UseVisualStyleBackColor = true;
@@ -60,36 +84,40 @@
             tabPage2.Location = new Point(4, 26);
             tabPage2.Name = "tabPage2";
             tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(792, 196);
+            tabPage2.Size = new Size(755, 169);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "tabPage2";
             tabPage2.UseVisualStyleBackColor = true;
             // 
             // txtResult
             // 
-            txtResult.Dock = DockStyle.Bottom;
-            txtResult.Location = new Point(0, 228);
+            txtResult.Dock = DockStyle.Fill;
+            txtResult.Location = new Point(0, 0);
             txtResult.Multiline = true;
             txtResult.Name = "txtResult";
-            txtResult.Size = new Size(800, 342);
-            txtResult.TabIndex = 8;
+            txtResult.Size = new Size(763, 307);
+            txtResult.TabIndex = 11;
             // 
             // FormMain
             // 
             AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 570);
-            Controls.Add(txtResult);
-            Controls.Add(tabControlMain);
+            ClientSize = new Size(763, 477);
+            Controls.Add(splitContainer1);
             Name = "FormMain";
             Text = "木工计算器";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
             tabControlMain.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
+        private SplitContainer splitContainer1;
         private TabControl tabControlMain;
         private TabPage tabPageDesk;
         private TabPage tabPage2;
